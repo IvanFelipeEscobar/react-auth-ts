198597354a9ce21b628da534b7b6e438623e2e15
Ivan Escobar <ivnescbr1989@gmail.com>
2023-08-24 20:48:21 -0400

    initial commit

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export default function Navbar() {
+  return (
+    <div className='navbar'>
+        <span className="brand">User Auth App</span>
+        <ul className="links">
+            <li className="link">
+                <img src="" alt="" className="avatar" />
+            </li>
+            <li className="link">John joeferogoggg</li>
+            <li className="link"></li>
+        </ul>
+    </div>
+  )
+}
