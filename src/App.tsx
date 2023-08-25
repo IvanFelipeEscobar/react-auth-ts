bc1161ea7dac7316fd93144f234fc0f15d251fa8
Ivan Escobar <ivnescbr1989@gmail.com>
2023-08-24 21:12:34 -0400

    material ui navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 
 import './App.css'
+import Navbar from './components/Navbar'
 
 function App() {
 
   return (
     <>
+    <Navbar/>
      <div className="hello">Hello World</div>
     </>
   )
